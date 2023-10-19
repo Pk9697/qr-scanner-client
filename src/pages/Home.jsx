539391cce6bb5245c63d9c36be0f8cd7883cd91e
Prashant Kumar <prashant.aman9@gmail.com>
2023-10-19 18:56:14 +0530

    Complete Scanning feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react'
+import QrReader from 'react-qr-reader'
+
 function Home() {
+  const [data, setData] = useState('No result')
+  const [open, setOpen] = useState(false)
+  const [error, setError] = useState(null)
+
+  const handleScan = (result) => {
+    if (result) {
+      setData(result)
+      setOpen(false)
+    }
+  }
+
+  const handleError = (err) => {
+    setError(err)
+  }
+
   return (
     <div className="container">
       {/* <button type="button">Switch</button> */}
@@ -12,12 +30,24 @@ function Home() {
       </div>
 
       <div className="qr-container">
-        <div className="qr-box"></div>
-        <p>Scanning Code...</p>
+        <div className="qr-box">
+          {open && (
+            <QrReader
+              delay={1000}
+              onScan={handleScan}
+              onError={handleError}
+              style={{ width: '100%', height: '100%' }}
+            />
+          )}
+        </div>
+        <p>{open ? 'Loading...' : data}</p>
+        {error && <p>{error}</p>}
       </div>
 
       <div>
-        <button type="button">Place Camera Code</button>
+        <button type="button" onClick={() => setOpen((prev) => !prev)}>
+          {!open ? 'Scan QR' : 'Stop Scanning'}
+        </button>
       </div>
     </div>
   )
