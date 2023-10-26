bfe02d63fe7bb364a134492a3ce89d5297fa05f1
Prashant Kumar <prashant.aman9@gmail.com>
2023-10-26 22:20:00 +0530

    Setup for production

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,4 +1,5 @@
-export const API_ROOT = 'http://127.0.0.1:4001/api/v1'
+export const API_ROOT =
+  import.meta.env.VITE_API_ROOT || 'http://127.0.0.1:4001/api/v1'
 
 export const APIUrls = {
   login: () => `${API_ROOT}/users/login`,
